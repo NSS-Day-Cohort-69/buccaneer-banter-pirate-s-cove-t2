41339d31ae38a29c14aef8553f0473b57ce32fac
Zachary Trouy <ztrouy@gmail.com>
2024-04-23 14:26:24 -0500

    Added initial properties

--- BuccaneerBanterAPI/Models/DTOs/PirateDTO.cs
+++ BuccaneerBanterAPI/Models/DTOs/PirateDTO.cs
@@ -2,5 +2,14 @@ namespace BuccaneerBanterAPI.Models.DTOs;
 
 public class PirateDTO
 {
-
+    public int Id {get; set;}
+    public string Name {get; set;}
+    public int? Age {get; set;}
+    public string? ImgURL {get; set;}
+    public int? NationalityId {get; set;}
+    public NationalityDTO? Nationality {get; set;}
+    public int RankId {get; set;}
+    public RankDTO Rank {get; set;}
+    public int ShipId {get; set;}
+    public ShipDTO Ship {get; set;}
 }
--- BuccaneerBanterAPI/Models/DTOs/ShipDTO.cs
+++ BuccaneerBanterAPI/Models/DTOs/ShipDTO.cs
@@ -2,5 +2,6 @@ namespace BuccaneerBanterAPI.Models.DTOs;
 
 public class ShipDTO
 {
-
+    public int Id {get; set;}
+    public string Name {get; set;}
 }
