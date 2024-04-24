3929221c5c40cb2f163a8f9308ae274a20d84af4
Zachary Trouy <ztrouy@gmail.com>
2024-04-24 10:14:01 -0500

    Changed property name

--- BuccaneerBanterAPI/Models/DTOs/FollowerDTO.cs
+++ BuccaneerBanterAPI/Models/DTOs/FollowerDTO.cs
@@ -4,6 +4,6 @@ public class FollowerDTO
 {
     public int Id {get; set;}
     public int PirateId {get; set;}
-    public PirateFollowerDTO PirateFollower {get; set;}
+    public PirateFollowerDTO Pirate {get; set;}
     public int FollowerId {get; set;}
 }
